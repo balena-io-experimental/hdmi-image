8a86aca58d9e74165b97a973d1b3e7797e67163b
Ronald McCollam <mccollam@gmail.com>
2016-08-24 14:35:54 -0400

    Remove driver for TFT display, switch to framebuffer 0

--- start.sh
+++ start.sh
@@ -2,6 +2,6 @@
 
 echo "rendering image"
 # cat /usr/src/app/images/image.raw > /dev/fb1
-fbi -d /dev/fb1 -T 1 -noverbose -a images/image.png
+fbi -d /dev/fb0 -T 1 -noverbose -a images/image.png
 
 sleep 9999999
